5b721af307bfdd60c7eb91531222c5824b28fbb9
YUN GU <2044582799@qq.com>
2026-07-18 16:48:34 +0800

    111

--- cyt4-bb7_-control_-middleware_-library-master/Simple_Template/code/System/usr_system.cpp
+++ cyt4-bb7_-control_-middleware_-library-master/Simple_Template/code/System/usr_system.cpp
@@ -512,7 +512,16 @@ void ComunicateTask(void *argument) {
       sendSpeedToMotor(left_vref, right_vref);
       control_ready = false;
     }
+<<<<<<< Updated upstream
 
+=======
+    //if(debug_print_f == true)
+    //{
+    //  printf(" l=%d, r=%d\r\n", 
+    //        (int)(target_linear_speed_l*100), (int)(target_linear_speed_r*100));
+    //  debug_print_f = false;
+    //}
+>>>>>>> Stashed changes
     
     vTaskDelay(1);
   }
