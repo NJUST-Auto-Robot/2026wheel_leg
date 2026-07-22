304cd6750213e7b9a2fe095b4c3ed96af9677a26
Peng-Cheng419 <m18875974721@163.com>
2026-07-22 14:04:23 +0800

    fix minor bugs

--- cyt4-bb7_-control_-middleware_-library-master/Simple_Template/user/cm7_0_isr.c
+++ cyt4-bb7_-control_-middleware_-library-master/Simple_Template/user/cm7_0_isr.c
@@ -268,7 +268,7 @@ void uart3_isr(void) {
 void uart4_isr(void) {
   if (Cy_SCB_GetRxInterruptMask(get_scb_module(UART_4)) & CY_SCB_UART_RX_NOT_EMPTY){ //串口4接收中断
     
-    uart4_callback(); 
+    //uart4_callback(); 
 
     // 清除接收中断标志位
     Cy_SCB_ClearRxInterrupt(get_scb_module(UART_4), CY_SCB_UART_RX_NOT_EMPTY);
@@ -280,15 +280,6 @@ void uart4_isr(void) {
 }
 
 void uart5_isr(void) {
-  if (Cy_SCB_GetRxInterruptMask(get_scb_module(UART_5)) & CY_SCB_UART_RX_NOT_EMPTY){ //串口5接收中断
-    
-    CRSF_callback(); 
-    // 清除接收中断标志位
-    Cy_SCB_ClearRxInterrupt(get_scb_module(UART_5), CY_SCB_UART_RX_NOT_EMPTY);
-  } 
-  else if (Cy_SCB_GetTxInterruptMask(get_scb_module(UART_5)) & CY_SCB_UART_TX_DONE){ //串口5发送中断
-    // 清除发送中断标志位
-    Cy_SCB_ClearTxInterrupt(get_scb_module(UART_5), CY_SCB_UART_TX_DONE);
-  }
+;
 }
 // **************************** 串口中断函数 ****************************
